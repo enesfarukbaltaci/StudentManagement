1d982af6d5650477360f723b02dd300c7d4bac75
efarukbaltaci <efarukbaltaci@gmail.com>
2024-02-12 21:32:00 +0100

    12.02

---
+++ src/main/java/com/project/contactmessage/controller/ContactMessageController.java
@@ -0,0 +1,2 @@
+package com.project.contactmessage.controller;public class ContactMessageController {
+}
---
+++ src/main/java/com/project/contactmessage/repository/ContactMessageRepository.java
@@ -0,0 +1,2 @@
+package com.project.contactmessage.repository;public interface ContactMessageRepository {
+}
